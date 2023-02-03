cc5e7acac6b570d76c7759f9bf8f4b85bbb08c0e
Dzmitry_Betska <d.betska@gmail.com>
2023-02-03 23:08:14 +0300

    adding homework files

---
+++ Itacademycourses/src/homework2/Task1.java
@@ -0,0 +1,15 @@
+package homework2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        byte b = 85;
+        short s = 22015;
+        int i = 1000000;
+        long l = 4294967295L;
+        char ch = 'a';
+        float f = 4.35678f;
+        double d = 67.9876452;
+        boolean bool = true;
+        System.out.printf("b=%d; \ns=%d; \ni=%d; \nl=%d; \nch=%c; \nf=%.5f; \nd=%.7f; \nbool=%s", b, s, i, l, ch, f, d, bool);
+    }
+}
